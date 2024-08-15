363d0b5ead88fe0878ca78b37e7222b204fd0d01
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-08-15 19:18:24 +0400

    0x00-python_variable_annotations

---
+++ 0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations
+FUnction to write type anotated function add
+Takes two floats a and b and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    type-annotated function
+    """
+    return a + b
---
+++ 1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - concat
+Write type-annotated function concat that takes two strings as argument
+Returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str):
+    """
+    type-annotated function
+    concat
+    """
+    return str1 + str2
